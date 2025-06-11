40744d0ce897edc521a47f2162e334526995da38
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2025-06-11 14:38:03 -0600

    Update attachmentcategory.service.ts
    Added basic functionality

--- src/attachmentcategory/attachmentcategory.service.ts
+++ src/attachmentcategory/attachmentcategory.service.ts
@@ -1,26 +1,52 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAttachmentcategoryDto } from './dto/create-attachmentcategory.dto';
 import { UpdateAttachmentcategoryDto } from './dto/update-attachmentcategory.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AttachmentcategoryService {
-  create(createAttachmentcategoryDto: CreateAttachmentcategoryDto) {
-    return 'This action adds a new attachmentcategory';
+  constructor(
+    private readonly prisma: PrismaService
+  ) {}
+
+  async create(createAttachmentcategoryDto: CreateAttachmentcategoryDto) {
+    const result = await this.prisma.attachmentcategory.create({
+      data: createAttachmentcategoryDto,
+    });
+    return result;
   }
 
-  findAll() {
-    return `This action returns all attachmentcategory`;
+  async bulkCreate(attachmentcategories: CreateAttachmentcategoryDto[]) {
+    const result = await this.prisma.attachmentcategory.createMany({
+      data: attachmentcategories,
+    });
+    return result;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} attachmentcategory`;
+  async findAll() {
+    const result = await this.prisma.attachmentcategory.findMany();
+    return result;
   }
 
-  update(id: number, updateAttachmentcategoryDto: UpdateAttachmentcategoryDto) {
-    return `This action updates a #${id} attachmentcategory`;
+  async findOne(id: number) {
+    const result = await this.prisma.attachmentcategory.findUnique({
+      where: { id },
+    });
+    return result;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} attachmentcategory`;
+  async update(id: number, updateAttachmentcategoryDto: UpdateAttachmentcategoryDto) {
+    const result = await this.prisma.attachmentcategory.update({
+      where: { id },
+      data: updateAttachmentcategoryDto,
+    });
+    return result;
+  }
+
+  async remove(id: number) {
+    const result = await this.prisma.attachmentcategory.delete({
+      where: { id },
+    });
+    return result;
   }
 }
